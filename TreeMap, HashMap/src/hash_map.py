01e93f2f137582889532b01692f5303eacf06d8d
Nikita Marin <nikita18.marin@gmail.com>
2022-03-27 15:06:15 +0300

    done pylint hw

--- TreeMap, HashMap/src/hash_map.py
+++ TreeMap, HashMap/src/hash_map.py
@@ -1,12 +1,28 @@
-class Node(object):
+"""Realisation of Linked list and HashMap"""
+
+
+class Node:
+    """class Node"""
+
     def __init__(self, data=None):
         # Информационная составляющая
         self.data = data
         # Ссылка на след узел
         self.next = None
 
+    def __str__(self):
+        return f'{self.data}'
+
+    def positive_data(self):
+        """checking that data >= 0"""
+        if self.data >= 0:
+            return True
+        return False
+
 
 class SingleList:
+    """class Linked List"""
+
     def __init__(self):
         self.head = None
         self.end = None
@@ -15,6 +31,7 @@ class SingleList:
         self.lenth = 0
 
     def add_item(self, item):
+        """add item"""
         item = Node(item)
         if self.head is None:
             self.head = item
@@ -24,30 +41,34 @@ class SingleList:
         self.lenth += 1
 
     def list_len(self):
+        """return length of linked list"""
         return self.lenth
 
     def print_list(self):
+        """print linked list"""
         now_node = self.head
         while now_node is not None:
             print(now_node.data)
             now_node = now_node.next
 
     def list_sum(self):
+        """return sum of all items in linked list"""
         now_node = self.head
-        s = None
+        list_sum = None
         while now_node is not None:
-            if isinstance(now_node.data, int) or isinstance(now_node.data, float):
-                if s is None:
-                    s = 0
-                s += now_node.data
+            if isinstance(now_node.data, (int, float)):
+                if list_sum is None:
+                    list_sum = 0
+                list_sum += now_node.data
             now_node = now_node.next
-        return s
+        return list_sum
 
     def find_max(self):
+        """find max element in linked list"""
         now_node = self.head
         max_num = None
         while now_node is not None:
-            if isinstance(now_node.data, int) or isinstance(now_node.data, float):
+            if isinstance(now_node.data, (int, float)):
                 if max_num is None:
                     max_num = -100000000000
                 if now_node.data > max_num:
@@ -56,25 +77,26 @@ class SingleList:
         return max_num
 
     def has_otr(self):
+        """checking for the presence of a negative element"""
         flag = True
         now_node = self.head
         while now_node is not None:
-            if isinstance(now_node.data, int) or isinstance(now_node.data, float):
+            if isinstance(now_node.data, (int, float)):
                 if now_node.data < 0:
                     flag = False
             now_node = now_node.next
         if flag:
-            return f'Не содержит отрицательных чисел'
-        return f'Есть отрицательное(ые) число'
+            return 'Не содержит отрицательных чисел'
+        return 'Есть отрицательное(ые) число'
 
     def delete_by_id(self, item_id):
+        """deleting an item by id"""
         now_id = 1
         now_node = self.head
         self.lenth -= 1
         prev_node = None
         while now_node is not None:
             if now_id == item_id:
-                # если хотим удлать элемент который уже встречался, то заменим на prev_node.data == item
                 if prev_node is not None:
                     prev_node.next = now_node.next
                 else:
@@ -92,6 +114,7 @@ class SingleList:
 
 
 class HashMap:
+    """class HashMap"""
     def __init__(self, _size=10):
         self._size = _size
         self._inner_list = [SingleList() for i in range(_size)]
@@ -119,13 +142,12 @@ class HashMap:
         raise KeyError
 
     def __str__(self):
-        s = ''
-        k = 0
+        print_s = ''
         for i in range(self._size):
             for j in self._inner_list[i]:
-                s += str(j)
-            s += '\n'
-        return s
+                print_s += str(j)
+            print_s += '\n'
+        return print_s
 
     def __len__(self):
         return self._length
@@ -137,5 +159,5 @@ class HashMap:
             self._length -= 1
 
     def size(self):
+        """return size of HashMap"""
         return self._size
-
--- TreeMap, HashMap/test/test_map.py
+++ TreeMap, HashMap/test/test_map.py
@@ -1,9 +1,12 @@
+"""test for HashMap and TreeMap"""
 import unittest
 from src.hash_map import HashMap
 from src.tree_map import TreeMap
 
 
 class TestMap(unittest.TestCase):
+    """General tests"""
+
     def setUp(self) -> None:
         self.hash_map = HashMap()
         self.tree_map = TreeMap()
@@ -12,34 +15,40 @@ class TestMap(unittest.TestCase):
         self.tree_map[4] = 13
 
     def test_get(self):
+        """get test"""
         self.assertEqual(self.hash_map[1], 19)
         self.assertEqual(self.hash_map['Nikita'], 'people')
         self.assertEqual(self.tree_map[4], 13)
 
     def test_key_error(self):
+        """test key error"""
         with self.assertRaises(KeyError):
             i = self.hash_map[100000]
+            del i
 
     def test_replace(self):
+        """test replace"""
         self.hash_map[1] = 20
         self.assertEqual(self.hash_map[1], 20)
         self.assertNotEqual(self.hash_map[1], 19)
 
 
 class TestHashMap(unittest.TestCase):
+    """Tests only for HashMap"""
+
     def setUp(self):
         self.hash_map = HashMap()
         self.hash_map[1] = 3
         self.hash_map[5] = 'kuku'
 
     def test_increase(self):
-        # размер должен увеличиться
+        """test increase"""
         self.hash_map[6] = 'wfwfw'
         self.hash_map['rere'] = 3
         self.assertEqual(self.hash_map.size(), 10)
 
     def test_not_increase(self):
-        # размер не должен увеличиться
+        """test not increase"""
         self.hash_map = HashMap(100)
         self.hash_map[1] = 1
         self.hash_map[2] = 2
@@ -49,34 +58,41 @@ class TestHashMap(unittest.TestCase):
         self.assertEqual(self.hash_map.size(), 100)
 
     def test_length(self):
+        """test length"""
         self.hash_map[6] = 'wfwfw'
         self.hash_map['rere'] = 3
         self.assertEqual(len(self.hash_map), 4)
 
     def test_delete(self):
+        """test delete"""
         del self.hash_map[5]
         self.assertEqual(len(self.hash_map), 1)
 
     def test_fake_delete(self):
-        # хэш мапа не поменяется после удаленеи элемента, которого не было
+        """test fake delete"""
         hash_map_was = self.hash_map
         del self.hash_map[1234]
         self.assertEqual(self.hash_map, hash_map_was)
 
 
 class TestTreeMap(unittest.TestCase):
+    """"Tests only for TreeMap"""
+
     def setUp(self):
         self.tree_map = TreeMap()
 
     def test_root_none(self):
+        """test root is none"""
         self.assertEqual(self.tree_map.root, None)
 
     def test_size(self):
+        """test size"""
         self.tree_map[1] = 12
         self.tree_map[45] = 34
         self.assertEqual(self.tree_map.size, 2)
 
     def test_replace(self):
+        """test replace"""
         self.tree_map[1] = 1
         self.tree_map[2] = 45
         self.tree_map[56] = 34
@@ -85,16 +101,15 @@ class TestTreeMap(unittest.TestCase):
         self.assertNotEqual(self.tree_map[2], 45)
 
     def test_replace_root_is_none(self):
-        # дерево меняется
+        """test where replaced root == none"""
         self.tree_map = TreeMap()
         self.tree_map[1] = 100
         self.assertNotEqual(self.tree_map.root, None)
 
     def test_tree_delete_node(self):
-        # можно задать со значением не None
+        """test where root is not None"""
         self.tree_map = TreeMap(6)
         self.assertNotEqual(self.tree_map.root, None)
-
 
 
 if __name__ == '__main__':
